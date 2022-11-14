360272f5b12319f2e86c2fb0774a6da1dfeb38e9
Philip Le <lephil505006@gmail.com>
2022-11-14 16:18:00 -0600

    Setting up js portion

--- js/site.js
+++ js/site.js
@@ -1,5 +1,26 @@
-fetch("https://weatherdbi.herokuapp.com/data/weather/lagrangepark")
-    .then(response => response.json())
-    .then(data => console.log(data))
+let weather = {
+    fetchWeather: function (city) {
+        fetch("https://weatherdbi.herokuapp.com/data/weather/" + city)
+            .then(response => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then(data => console.log(data))
+    }
+}
 
+document.querySelector("button").addEventListener("click", function () {
+    weather.fetchWeather(document.querySelector(".search-bar").value);
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.fetchWeather(document.querySelector(".search-bar").value);
+        }
+    });
 
